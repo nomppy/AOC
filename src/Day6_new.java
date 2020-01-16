50e2da84468c944efe753b46417657914e632e95
Kenneth S <44417854+spesthecat@users.noreply.github.com>
2020-01-15 19:20:37 -0500

    Soooooo close ... just need to fix tree intake for when the nodes are out of order.

--- src/Day6_new.java
+++ src/Day6_new.java
@@ -1,8 +1,32 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 class Day6_new {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        Scanner aoc = new Scanner(new BufferedReader(new FileReader("data6.txt")));
+        Scanner test = new Scanner(new BufferedReader(new FileReader("test.txt")));
 
+        Root root = new Root();
+        while(test.hasNextLine()){
+            String orbit = test.nextLine();
+            String child = orbit.split("\\)")[1];
+            String parent = orbit.split("\\)")[0];
+
+            Root c = root.get(child);
+            Root p = root.get(parent);
+
+            if (c == null && p == null){ // neither child nor parent are in tree
+                new Root(parent, 0).addChild(child);
+            }else if (c != null && p == null){ // child is in tree but parent is not
+                new Root(parent, c.getHeight() - 1).addChild(c);
+            }else if(root.get(child) == null && root.get(parent) != null){ // parent is in tree but child is not
+                p.addChild(child);
+            }else if(root.get(child) != null && root.get(parent) != null){ // both are in tree
+                p.addChild(c);
+            }
+            root.updateAll();
+        }
+        System.out.println(root.subtree());
     }
 }
 
@@ -15,12 +39,17 @@ class Root {
 
     // constructor
 
+    public Root(){
+
+    }
+
     public Root(Root node){
         this.name = node.getName();
         this.height = node.getHeight();
         this.children = node.getChildren();
         this.parent = node.getParent();
     }
+
     public Root(String name, Root parent){
         this.name = name;
         this.parent = parent;
@@ -47,29 +76,38 @@ class Root {
 
         if (name.equals(this.getName())) return this;
         ArrayList<Root> next;
-        for (int i = this.getHeight() + 1; i < size; i ++){
+        for (int i = this.getHeight() + 1; i <= size; i ++){
             next = this.getLayer(i);
             for (Root n : next) {
-                if (n.getName() == name) {
+                if (n.getName().equals(name)) {
                     return n;
                 }
             }
         }
-        System.out.println("Could not find node " + name);
         return null;
     }
 
-    private ArrayList<Root> getLayer(int layer){
+    public ArrayList<Root> nextLayer(){
+        return this.getLayer(this.getHeight() + 1);
+    }
+
+    public ArrayList<Root> getLayer(int layer){
+
         // returns an ArrayList<Root> indexed with this at 0
-        if (layer > size || layer <= this.getHeight()) System.out.println("Invalid layer number.");
+        // layer means a certain layer of descendants from this
+
+        if (layer > size) System.out.println("Invalid layer number.");
 
         ArrayList<Root> nodes = new ArrayList<>();
         nodes.add(this);
+        ArrayList<Root> tmp = new ArrayList<>();
         for (int i = 0; i < layer; i ++){
-            for (int j = 0; j < nodes.size(); j ++){
-                nodes.addAll(nodes.get(j).getChildren());
-                nodes.remove(nodes.get(j));
+            for (Root node : nodes) {
+                tmp.addAll(node.getChildren());
             }
+            nodes.clear();
+            nodes.addAll(tmp);
+            tmp.clear();
         }
         return nodes;
     }
@@ -78,26 +116,44 @@ class Root {
         return children;
     }
 
-    public void updateChildren(){
+    public void updateAll(){
+
+        // update relationships between all nodes in the current subtree
         for (Root n : children){
             n.update();
-            n.updateChildren();
+            n.updateAll();
         }
+    }
+
+    public void addChild(String name){
+        children.add(new Root(name, this));
     }
 
     public void addChild(Root child){
         children.add(child);
+        child.setParent(this);
     }
 
     public void update(){
         height = parent.getHeight() + 1;
     }
 
+
+
+    public ArrayList<Root> subtree(){
+        ArrayList<Root> tree = new ArrayList<>();
+        for (int i = 0; i < size; i ++){
+            tree.addAll(this.getLayer(i));
+        }
+        return tree;
+    }
+
     public String toString(){
         try {
             return "Name: " + name + "  Height: " + height + "  Parent: " + parent.getName();
         }catch (Exception NullPointerException){
-            return "Name: " + name + "  Height: " + height + "  Parent: " + "None";        }
+            return "Name: " + name + "  Height: " + height + "  Parent: " + "None";
+        }
     }
 
 
